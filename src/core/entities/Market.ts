4743a4b412b5a0667be7b3d0c90bb29b49f06c06
Daniel Padrino <danswarrior1@gmail.com>
2022-05-03 17:13:00 -0300

    Market interface amend

--- src/core/entities/Market.ts
+++ src/core/entities/Market.ts
@@ -1,11 +1,6 @@
 import Advertise from "./Advertise";
-import Pair from "./Pair";
-import TypeOperation from "./TypeOperation";
-
 interface Market {
-    pair: Pair;
-    typeOperation: TypeOperation
-    getAdvertises: Promise<Advertise[]>;
+    getAdvertises(): Promise<Advertise[]>;
 }
 
 export default Market
