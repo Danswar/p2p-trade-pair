2156200fc032732161d24250f8b9936f7045e8b6
Daniel Padrino <danswarrior1@gmail.com>
2022-05-05 15:49:53 -0300

    move mocked market to correct place

---
+++ src/core/infra/MockedMarketProvider.ts
@@ -0,0 +1,31 @@
+import Market from "../entities/Market";
+import Advertise from "../entities/Advertise";
+import TypeOperation from "../entities/TypeOperation";
+import Pair from "../entities/Pair";
+
+// Mocked by now
+const market: Market = {
+    getAdvertises: async () => {
+        const ads: Advertise[] = [
+            {
+                id: "id",
+                advertiser: {
+                    name: "seller name",
+                    tradeCount: 500,
+                    score: 100
+                },
+                price: 100,
+                typeOperation: TypeOperation.SELL,
+                pair: new Pair("BTC", "ARS"),
+                minAmount: 10,
+                maxAmount: 100,
+                paymentChannels: ["Brubank"],
+                publicView: new URL("https://www.someprovider.com/seller"),
+                isValid: () => true
+            }
+        ]
+        return ads
+    }
+}
+
+export default market
--- src/core/useCases/getBestAds/index.ts
+++ src/core/useCases/getBestAds/index.ts
@@ -1,34 +1,6 @@
 import GetBestAds, { GetBestAdsInputPort } from "./GetBestAds";
-import Market from "../../entities/Market";
-import Advertise from "../../entities/Advertise";
-import TypeOperation from "../../entities/TypeOperation";
-import Pair from "../../entities/Pair";
+import mockedMarketProvider from "../../infra/MockedMarketProvider";
 
-// Mocked by now
-const market: Market = {
-    getAdvertises: async () => {
-        const ads: Advertise[] = [
-            {
-                id: "id",
-                advertiser: {
-                    name: "seller name",
-                    tradeCount: 500,
-                    score: 100
-                },
-                price: 100,
-                typeOperation: TypeOperation.SELL,
-                pair: new Pair("BTC", "ARS"),
-                minAmount: 10,
-                maxAmount: 100,
-                paymentChannels: ["Brubank"],
-                publicView: new URL("https://www.someprovider.com/seller"),
-                isValid: () => true
-            }
-        ]
-        return ads
-    }
-}
-
-const getBestAds = new GetBestAds(market)
+const getBestAds = new GetBestAds(mockedMarketProvider)
 
 export default (params: GetBestAdsInputPort) => getBestAds.execute(params)
