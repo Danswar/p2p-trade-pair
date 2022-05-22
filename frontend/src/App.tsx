0d4fe36f700132f6c665f11ad2f6bc4a0020e253
Daniel Padrino <danswarrior1@gmail.com>
2022-05-22 12:20:34 -0300

    some elements in the screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
 import Layout from "./components/Layout";
 import AppBar from "./components/AppBar";
 
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Box from "@mui/system/Box";
+
+const useQueryBestAds = () => {};
+
 function App() {
   return (
     <Layout>
       <AppBar title="P2P Manager" />
+      <Box component="form">
+        <Box>
+          <TextField label="monto" variant="standard" />
+          <TextField label="tasa" variant="standard" />
+        </Box>
+        <Button onClick={() => {}} variant="contained">
+          Buscar
+        </Button>
+      </Box>
     </Layout>
   );
 }
