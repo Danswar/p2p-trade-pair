602725c3fefce428982c1f64ee896ded214b1796
Daniel Padrino <danswarrior1@gmail.com>
2022-06-07 20:00:43 -0300

    fetch ads

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Layout from "./components/Layout";
 import AppBar from "./components/AppBar";
 
@@ -5,9 +6,37 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Box from "@mui/system/Box";
 
-const useQueryBestAds = () => {};
+const BEST_ADS_URL = `http://localhost:3000`;
+
+type FetchAdsInput = {
+  typeOperation: string;
+  from: string;
+  to: string;
+  amount: string;
+};
+
+const fetchAds = async ({ typeOperation, from, to, amount }: FetchAdsInput) => {
+  const res = await fetch(
+    `${BEST_ADS_URL}/${typeOperation}/${from}/${to}/${amount}`
+  );
+  const { data } = await res.json();
+  return data;
+};
 
 function App() {
+  const [ads, setAds] = useState([]);
+
+  const searchAds = async () => {
+    setAds(
+      await fetchAds({
+        typeOperation: "sell",
+        from: "usdt",
+        to: "btc",
+        amount: "100"
+      })
+    );
+  };
+
   return (
     <Layout>
       <AppBar title="P2P Manager" />
@@ -16,9 +45,14 @@ function App() {
           <TextField label="monto" variant="standard" />
           <TextField label="tasa" variant="standard" />
         </Box>
-        <Button onClick={() => {}} variant="contained">
+        <Button onClick={searchAds} variant="contained">
           Buscar
         </Button>
+      </Box>
+      <Box>
+        {ads.map((ad: { id: string }) => (
+          <div>{ad.id}</div>
+        ))}
       </Box>
     </Layout>
   );
