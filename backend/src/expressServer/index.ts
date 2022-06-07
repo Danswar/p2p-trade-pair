0dd7749afefee40690bc95020a34071e5ba8d097
Daniel Padrino <danswarrior1@gmail.com>
2022-06-07 20:00:23 -0300

    enable cors

--- backend/src/expressServer/index.ts
+++ backend/src/expressServer/index.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import adsController from './controllers/ads.controller'
 
 export const initExpressServer = () => {
   const app = express()
   const port = 3000
+
+  app.use(cors())
 
   app.get('/:typeOperation/:from/:to/:amount', adsController)
 
